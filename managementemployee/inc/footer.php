2e3c70f7436e544cbb54aaeb80ae5f38cae46fdf
Ranjan123Acharya <108257328+Ranjan123Acharya@users.noreply.github.com>
2022-10-03 14:25:03 +0545

    Add files via upload

---
+++ managementemployee/inc/footer.php
@@ -0,0 +1,34 @@
+<div class="container-fluid bg-white mt-5 shadow-lg">
+    <div class="row">
+        <div class="col-lg-4 p-4 shadow">
+            <h3 class="h-font fw-bold fs-3 mb-2">Employee</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, aut?</p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="h-font fw-bold fs-3 mb-2">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">New Employee</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">View/Update Employee</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Employee Search</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Employee History</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Department Information</a> <br>
+
+        </div>
+
+        <div class="col-lg-4 p-4 shadow">
+            <h3 class="h-font fw-bold fs-3 mb-2">Group Member</h3>
+            <p>First Person (Student id)</p>
+            <p>Second Person (Student id)</p>
+            <p>Third Person (Student id)</p>
+            <p>Fourth Person (Student id)</p>
+
+        </div>
+
+
+    </div>
+</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
